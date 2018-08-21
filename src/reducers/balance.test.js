74d08084c7b481188889fb4adcf473df370b241c
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-21 15:51:02 -0300

    Store data with cookies

--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,16 +1,29 @@
 import balanceReducer from './balance'
+import balanceReducer2 from './balance'
 import * as constants from '../actions/constants'
 
 describe('Balance reducer', () => {
-  test('sets a balance', () => {
+  describe('when initializing', () => {
     const balance = 10
-    const state = balanceReducer({}, {
-      type: constants.SET_BALANCE,
-      balance,
-    })
 
-    expect(state).toEqual(balance)
+    test('sets a balance', () => {
+      const state = balanceReducer(undefined, {
+        type: constants.SET_BALANCE,
+        balance,
+      })
+  
+      expect(state).toEqual(balance)
+    })
+  
+    describe('then re-initializing', () => {
+      test('reads the balance from cookies', () => {
+        const state = balanceReducer2(undefined, {})
+  
+        expect(state).toEqual(balance)
+      })
+    })
   })
+
 
   test('deposits into the balance', () => {
     const deposit = 10
