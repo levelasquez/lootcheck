2dc7a5518c2ebbdaeb505f7fabf48897aeb37489
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-21 10:57:13 -0300

    Set a balance with a redux action

---
+++ src/actions/balance.test.js
@@ -0,0 +1,13 @@
+import * as constants from './constants'
+import * as actions from './balance'
+
+test('creates an action to set the balance', () => {
+  const balance = 0
+  const expectedAction = {
+    type: constants.SET_BALANCE,
+    balance,
+  }
+  const result = actions.setBalance(balance)
+
+  expect(result).toEqual(expectedAction)
+})
