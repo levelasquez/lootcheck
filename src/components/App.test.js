863b3dc951c3b5320201a49cbe4e816dc8b245b8
Leonardo Velasquez <velasquezleonardo1@gmail.com>
2018-08-21 11:25:28 -0300

    Render the wallet

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -6,4 +6,10 @@ describe('App component', () => {
   const app = shallow(<App />)
 
   test('renders properly', () => expect(app).toMatchSnapshot())
+
+  test('contains a connected Wallet component', () => {
+    const exists = app.find('Connect(Wallet)').exists()
+
+    expect(exists).toBe(true)
+  })
 })
